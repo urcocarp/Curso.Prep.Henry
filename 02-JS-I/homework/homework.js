71a17f98ac70be74d1bd8b0b7fb25addeb46fb57
urco <nik_186@hotmail.com>
2023-01-05 21:49:30 -0300

    homeworksesvocal

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -281,10 +281,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.length!==1) return "Dato incorrecto"
-  else if(letra==="a"|| letra==="e"||letra==="i"||letra==="o"||letra==="u"){
-    return "Es vocal"
-  } else {return "Dato incorrecto"}
+ if (letra.length > 1){
+  return "Dato incorrecto"
+ }
+  if(letra === "a" || letra=== "e" || letra=== "i" || letra=== "o" || letra === "u"){
+return "Es vocal"
+ } else {
+  return "Dato incorrecto"
+ }
+
 } 
 
 
